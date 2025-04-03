b4fb29740251e89c8deaf116767fd8a8aa6d08cf
lowcaidol <89917647+lowcaidol@users.noreply.github.com>
2025-04-04 00:13:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-// Funkcja, która będzie sprawdzać, kiedy element jest widoczny
+// Funkcja, która sprawdza widoczność elementów i animuje karty
 const observer = new IntersectionObserver((entries, observer) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      entry.target.classList.add("visible");  // Dodaje klasę visible
-      observer.unobserve(entry.target);  // Przestaje obserwować ten element po animacji
+      entry.target.classList.add("visible");
+      observer.unobserve(entry.target);
     }
   });
 }, {
-  threshold: 0.5  // Element będzie uznawany za "widoczny", gdy 50% elementu pojawi się na ekranie
+  threshold: 0.5
 });
 
-// Obserwowanie wszystkich kart pakietów
-const cards = document.querySelectorAll('.pricing-card'); 
+// Obserwacja kart
+const cards = document.querySelectorAll('.card');
 cards.forEach(card => {
-  observer.observe(card);  // Rozpoczyna obserwację
+  observer.observe(card);
+});
+
+// Funkcja, która pokaże odpowiedź w FAQ po najechaniu na pytanie
+const faqQuestions = document.querySelectorAll('.faq-q');
+faqQuestions.forEach(question => {
+  question.addEventListener('mouseover', function() {
+    this.nextElementSibling.style.display = "block";
+  });
+  question.addEventListener('mouseout', function() {
+    this.nextElementSibling.style.display = "none";
+  });
 });
